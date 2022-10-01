833bfede053da376a34b5c4483a726bac2361be9
kuda <konstantin.kuda@un.org>
2022-10-01 10:33:47 +0200

    ClimbStairs solution added

--- LeetCode.Solutions.Tests/Others/ClimbStairsTests.cs
+++ LeetCode.Solutions.Tests/Others/ClimbStairsTests.cs
@@ -15,9 +15,19 @@ public class ClimbStairsTests
     
     [TestCase(2, 2)]
     [TestCase(3, 3)]
+    [TestCase(5, 8)]
     public void FindTest(int input, int output)
     {
         var result = _solution.Find(input);
+        result.Should().Be(output);
+    }
+    
+    [TestCase(2, 2)]
+    [TestCase(3, 3)]
+    [TestCase(5, 8)]
+    public void Find2Test(int input, int output)
+    {
+        var result = _solution.Find2(input);
         result.Should().Be(output);
     }
 }
--- LeetCode.Solutions/Others/ClimbStairs.cs
+++ LeetCode.Solutions/Others/ClimbStairs.cs
@@ -6,6 +6,39 @@ public class ClimbStairs
 {
     public int Find(int n)
     {
-        throw new NotImplementedException();
+        if (n <= 0)
+        {
+            return 0;
+        }
+        else if (n <= 2)
+        {
+            return n;
+        }
+
+        return Find(n - 1) + Find(n - 2);
+    }
+
+    public int Find2(int n)
+    {
+        if (n <= 0)
+        {
+            return 0;
+        }
+        if (n <= 2)
+        {
+            return n;
+        }
+
+        var first = 1;
+        var second = 2;
+
+        for (int i = n - 2 ; i > 1 ; i--)
+        {
+            var temp = second;
+            second += first;
+            first = temp;
+        }
+
+        return first + second;
     }
 }
