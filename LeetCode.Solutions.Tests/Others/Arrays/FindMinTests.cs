28ddb517034494b443161f61202767d976e0e631
kuda <konstantin.kuda@un.org>
2023-06-28 11:42:50 +0200

    FindMin solution added

---
+++ LeetCode.Solutions.Tests/Others/Arrays/FindMinTests.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Solutions.Tests.Others.Arrays;
+
+[TestFixture]
+public class FindMinTests
+{
+    private FindMin _solution = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        _solution = new FindMin();
+    }
+    
+    [TestCase(new[]{3,4,5,1,2}, 1)]
+    [TestCase(new[]{4,5,6,7,0,1,2}, 0)]
+    [TestCase(new[]{11,13,15,17}, 11)]
+    [TestCase(new[]{5,1,2,3,4}, 1)]
+    public void Find1Test(int[] inputArray, int expectedResult)
+    {
+        var result = _solution.Find1(inputArray);
+        result.Should().Be(expectedResult);
+    }
+    
+    [TestCase(new[]{3,4,5,1,2}, 1)]
+    [TestCase(new[]{4,5,6,7,0,1,2}, 0)]
+    [TestCase(new[]{11,13,15,17}, 11)]
+    [TestCase(new[]{5,1,2,3,4}, 1)]
+    public void Find2Test(int[] inputArray, int expectedResult)
+    {
+        var result = _solution.Find2(inputArray);
+        result.Should().Be(expectedResult);
+    }
+}
---
+++ LeetCode.Solutions/Others/Arrays/FindMin.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCode.Solutions.Others.Arrays;
+
+// 153. Find Minimum in Rotated Sorted Array
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+public class FindMin
+{
+    // brute-force
+    public int Find1(int[] nums)
+    {
+        if (nums.Length == 1)
+        {
+            return nums[0];
+        }
+
+        var min = nums[0];
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] < min)
+            {
+                min = nums[i];
+            }
+        }
+
+        return min;
+    }
+
+    public int Find2(int[] nums)
+    {
+        if (nums.Length == 1)
+        {
+            return nums[0];
+        }
+        
+        var low = 0;
+        var high = nums.Length - 1;
+        
+        while (low < high)
+        {
+            var middle = low + (high - low) / 2;
+            if (nums[middle] > nums[high])
+            {
+                low = middle + 1;
+            }
+            else
+            {
+                high = middle;
+            }
+        }
+
+        return nums[high];
+    }
+}
