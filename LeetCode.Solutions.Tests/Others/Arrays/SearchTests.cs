fa90bdac1c64202640b77d185cd795a7d4f478d7
kuda <konstantin.kuda@un.org>
2023-07-06 15:49:05 +0200

    33. Search in Rotated Sorted Array

---
+++ LeetCode.Solutions.Tests/Others/Arrays/SearchTests.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Solutions.Tests.Others.Arrays;
+
+[TestFixture]
+public class SearchTests
+{
+    private Search _solution = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        _solution = new Search();
+    }
+
+    [TestCase(new[] { 4, 5, 6, 7, 0, 1, 2 }, 0, 4)]
+    [TestCase(new[] { 4, 5, 6, 7, 0, 1, 2 }, 3, -1)]
+    [TestCase(new[] { 1 }, 0, -1)]
+    [TestCase(new[] { 5, 1, 2, 3, 4 }, 1, 1)]
+    [TestCase(new[] { 8, 9, 2, 3, 4 }, 9, 1)]
+    public void FindIndex_BestCase(int[] nums, int target, int expectedIndex)
+    {
+        var result = _solution.FindIndex(nums, target);
+        result.Should().Be(expectedIndex);
+    }
+}
---
+++ LeetCode.Solutions/Others/Arrays/Search.cs
@@ -0,0 +1,69 @@
+﻿namespace LeetCode.Solutions.Others.Arrays;
+
+// 33. Search in Rotated Sorted Array
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
+public class Search
+{
+    public int FindIndex(int[] nums, int target)
+    {
+        if (nums.Length == 0)
+        {
+            return -1;
+        }
+        if (nums.Length == 1)
+        {
+            return nums[0] == target ? 0 : -1;
+        }
+
+        var left = 0;
+        var right = nums.Length - 1;
+        
+
+        while (left < right)
+        {
+            var leftValue = nums[left];
+            if (leftValue == target)
+            {
+                return left;
+            }
+            
+            var rightValue = nums[right];
+            if (rightValue == target)
+            {
+                return right;
+            }
+
+            var middle = (left + right) / 2;
+            var middleValue = nums[middle];
+
+            if (middleValue == target)
+            {
+                return middle;
+            }
+
+            if (leftValue < middleValue)
+            {
+                if (leftValue <= target && target < middleValue)
+                {
+                    right = middle - 1;
+                }
+                else
+                {
+                    left = middle + 1;
+                }
+            }
+            else
+            {
+                if (target > leftValue || target <= middleValue)
+                {
+                    right = middle - 1;
+                }
+                else
+                {
+                    left = middle + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
